19e4f8f523f0a1c4bf4ef1f2930ca4d5e56dc2fd
Roland <rolisz@yahoo.com>
2013-11-04 22:18:04 +0200

    Forgot a lot of stuff

--- Crypto/labs/l2/l2.cpp
+++ Crypto/labs/l2/l2.cpp
@@ -1,6 +1,112 @@
 #include <iostream>
+#include "InfInt.h"
+#include <vector>
+#include <algorithm>  
+using namespace std;
+
+vector<InfInt> trial_division_primes(InfInt n) {
+    InfInt sqrt_n = n.intSqrt();
+    vector<InfInt> divizori;
+	InfInt i = 2;
+	if (n % i == 0) {
+		divizori.push_back(i);
+	}
+	i = 3;
+    while (i <= sqrt_n) {
+        if ((n % i) == 0) {
+            divizori.push_back(i);
+        }
+        i+=2;
+    }
+    return divizori;
+}
+
+InfInt prime_factorizations(InfInt a, InfInt b) {
+	vector<InfInt> div_a = trial_division_primes(a);
+	vector<InfInt> div_b = trial_division_primes(b);
+	vector<InfInt>::iterator it;
+	int size;
+	if (div_a.size() > div_b.size()) {
+		size = div_a.size();
+	}
+	else {
+		size = div_b.size();
+	}
+	vector<InfInt> div_c(size);
+	it = set_intersection(div_a.begin(), div_a.end(), div_b.begin(), div_b.end(), div_c.begin());
+	div_c.resize(it-div_c.begin()); 
+	InfInt div = 1;
+	for (it=div_c.begin(); it!=div_c.end(); ++it) {
+		div *= *it;
+	}
+	return div;
+}
+
+InfInt euclid(InfInt a, InfInt b) {
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+	InfInt temp;
+    while (a > 0) {
+		temp = a;
+        a = b%a;
+		b = temp;
+	}
+    return b;
+}
+
+InfInt stein(InfInt num1, InfInt num2)
+{
+	InfInt pof2, tmp;
+	if (num1 == 0) {
+		return num2;
+	}
+	if (num2 == 0) {
+		return num2;
+	}
+
+	/* cea mai mare putere a lui 2 care divide ambele nr*/
+	pof2 = 0;
+	while(!(num1 % 2 == 1) && !(num2 % 2 == 1)) {
+		num1 /= 2;
+		num2 /= 2;
+		pof2++;
+	}
+
+	do {
+		while (!(num1 % 2 == 1)) {
+			num1 /= 2;
+		}
+		while (!(num2 % 2 == 1)) {
+			num2 /= 2;
+		}
+		/* num1 si num2 sunt impare*/
+		if (num1 >= num2) {
+			num1 = (num1 - num2) / 2;
+		}
+		else {
+			tmp = num1;
+			num1 = (num2 - num1) / 2;
+			num2 = tmp;
+		}
+	} while (!(num1 == num2 || num1 == 0));
+
+	while (pof2 > 0) {
+		num2 *=2;
+		pof2--;
+	}
+	return num2;
+}
 
 int main() {
-    std::cout<<"Hello world!";
-    return 0;
+	//generate_prime_list(65536);
+	while (!cin.eof()) {
+		InfInt nr1,nr2;
+		cin>>nr1;
+		cin>>nr2;
+		//cout<<"Prime factorization: "<<prime_factorizations(nr1, nr2)<<endl;
+		cout<<"Euclid: "<<euclid(nr1, nr2)<<endl;
+		cout<<"Stein: "<<stein(nr1, nr2)<<endl;
+	}
 }
---
+++ LFTC/L2/fsm.py
@@ -0,0 +1,90 @@
+from pprint import pprint
+
+__author__ = 'Roland'
+import sys
+
+def parse_automaton(descr):
+    states = set()
+    transitions = {}
+    alphabet = set()
+    finals = set()
+    begin_state = ""
+    for line in descr:
+        line = line.strip().split()
+        states.add(line[0])
+        states.add(line[2])
+        if len(line[1]) == 3 and line[1][1] == '-':
+            begin = ord(line[1][0])
+            end = ord(line[1][2])
+            alphabet.update(chr(x) for x in range(begin, end + 1))
+            for x in range(begin, end + 1):
+                transitions[(line[0], chr(x))] = line[2]
+        else:
+            alphabet.add(line[1])
+            transitions[(line[0], line[1])] = line[2]
+        if len(line) == 4:
+            if line[3] == "B":
+                begin_state = line[0]
+            elif line[3] == "F":
+                finals.add(line[2])
+
+    return begin_state, states, alphabet, transitions, finals
+
+def accept(begin_state, states, alphabet, transitions, finals, string):
+    current_state = begin_state
+    full_string = string
+    prefix = ""
+    temp_match = ""
+    while string:
+        current_char = string[0]
+        string = string[1:]
+        try:
+            next_state = transitions[(current_state, current_char)]
+        except KeyError:
+            return "Secventa %s nu este acceptata de acest automat finit, nu s-a putut consuma tot sirul! Prefixul " \
+                   "maximal consumat este: %s" % (full_string, prefix)
+        temp_match += current_char
+        if next_state in finals:
+            prefix += temp_match
+            temp_match = ""
+
+        current_state = next_state
+    if current_state in finals:
+        return "Secventa %s este acceptata" % full_string
+    else:
+        return "Secventa %s nu este acceptata, nu s-a ajuns intr-o stare terminala! Prefixul " \
+                   "maximal consumat este: %s" % (full_string, prefix)
+
+
+if len(sys.argv) > 1:
+    f = open(sys.argv[1], "rb")
+    descr = f.readlines()
+    f.close()
+else:
+    descr = []
+    text = raw_input("Dati automatul finit. Pentru oprire lasati linia goala: ")
+    while len(text):
+        descr.append(text)
+        text = raw_input()
+
+#print(descr)
+automaton = parse_automaton(descr)
+#print(begin_state)
+#print(states)
+#print(alphabet)
+#print(finals)
+#pprint(transitions)
+#check_string = raw_input("Dati secventa de verificat: ")
+check_string = "0xA23F"
+print(accept(*automaton, string=check_string))
+print(accept(*automaton, string="123"))
+print(accept(*automaton, string="xyx"))
+print(accept(*automaton, string="01234"))
+print(accept(*automaton, string="123ll"))
+print(accept(*automaton, string="123l"))
+print(accept(*automaton, string="0x123l"))
+print(accept(*automaton, string="0x123u"))
+print(accept(*automaton, string="0x123LL"))
+print(accept(*automaton, string="0x123xL"))
+print(accept(*automaton, string="0x"))
+print(accept(*automaton, string="0"))
